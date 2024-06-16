8095f55704f6d4b8c73dbad920bde7efa66e2554
Saparmurat-web <muratprivet2020@gmail.com>
2024-06-16 17:35:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,145 +1,147 @@
 import { Player } from "./player.js";
-import { Projecttile } from "./projectile.js";
+import { Projectile } from "./projectile.js";
 import { Enemy } from "./enemy.js";
 import { distanceBetweenTwoPoints } from "./utilities.js";
 
-const canvas = document.querySelector("canvas");
-const context = document.getContext("2d");
-const a = document.querySelector(".score-wrap");
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+const a = document.querySelector('.score-wrap');
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
 
-const scoreEl = document.querySelector("#score");
-const wastedElement = document.querySelector(".wasted");
+const scoreEl = document.querySelector('#score');
+const wastedElement = document.querySelector('.wasted');
 
 let player;
 let projectiles = [];
 let enemies = [];
 let particles = [];
 let score = 0;
-let annulation;
+let animationId;
 let countIntervalId;
 let spawnIntervalId;
 var audio = new Audio();
-    audio.preload = "auto";
-    audio.src = "shot.mp3";
-
+    audio.preload = 'auto';
+    audio.src = 'shot.mp3';
 var audioDeath = new Audio();
-    audioDeath.preload = "auto";
-    audioDeath.src = "death.mp3";
+    audioDeath.preload = 'auto';
+    audioDeath.src = 'death.mp3';
 
-document.addEventListener("click", () => {
-    audio = 0;
-    audio = new Audio();
-    audio.preload = "auto";
-    audio.src = "shot.mp3";
-    audio.play();
+document.addEventListener('click', () => {
+  audio = 0;
+  audio = new Audio();
+    audio.preload = 'auto';
+    audio.src = 'shot.mp3';
+  audio.play();
 })
 
-startGame ();
+startGame();
+
 function startGame() {
-    init();
-    animate();
-    spawnEnemies();
+  init();
+  animate();
+  spawnEnemies();
 }
 
+
 function init() {
-    const monementLimits = {
-        minX: 0,
-        maxX: canvas.width,
-        minY: 0,
-        maxY: canvas.height,
-    };
-    player = new Player(canvas.width/2, canvas.height/2, context, movemntLimits);
-    addEventListener("click", createProjectile);
+  const movementLimits = {
+    minX: 0,
+    maxX: canvas.width,
+    minY: 0,
+    maxY: canvas.height,
+  };
+  player = new Player(canvas.width / 2, canvas.height / 2, context, movementLimits);
+  addEventListener("click", createProjectile);
 }
 
 function createProjectile(event) {
-    projectiles.push(
-    new projectiles(
-        player.x,
-        player.y,
-        event.clientX,
-        event.clientY
-        )
+  projectiles.push(
+    new Projectile(
+      player.x,
+      player.y,
+      event.clientX,
+      event.clientY,
+      context
     )
-}
+  );
+};
 
 function spawnEnemies() {
-    let countOfSpawnEnemies = 1;
+  let countOfSpawnEnemies = 1;
 
-    countIntervalId = setInterval(() => countOfSpawnEnemies++, 30000);
-    spawnIntervalId = setInterval(() => spawnCountEnemies(countOfSpawnEnemies), 1000); //Можно изменять значение для того чтобы контроливать спавн врагов
+  countIntervalId = setInterval(() => countOfSpawnEnemies++, 30000);
+  spawnIntervalId = setInterval(() => spawnCountEnemies(countOfSpawnEnemies), 1000);
 
-    spawnCountEnemies(countOfSpawnEnemies);
+  spawnCountEnemies(countOfSpawnEnemies)
 }
 
 function spawnCountEnemies(count) {
-    for (let i = 0; i < count; i++) {
+  for (let i = 0; i < count; i++) {
     enemies.push(new Enemy(canvas.width, canvas.height, context, player));
-    }
+  }
 }
 
 function animate() {
-    animationId = requestAnimationFrame(animate);
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  animationId = requestAnimationFrame(animate);
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-    particles = particles.filter(particle => particle.alpha > 0);
-    projectiles = projectiles.filter(projectileInsideWindow);
-    enemies.forEach(enemy => checkHittingEnemies);
-    enimies = enemies.filter(enemy = enemy.health > 0);
-    const isGameOver = enemies.some(checkHittingEnemy) 
-    if (isGameOver) {
-        document.addEventListener("click", () => {
-            audio.pause();
+  particles = particles.filter(particle => particle.alpha > 0);
+  projectiles = projectiles.filter(projectileInsideWindow);
+  enemies.forEach(enemy => checkHittingEnemy(enemy));
+  enemies = enemies.filter(enemy => enemy.health > 0);
+  const isGameOver = enemies.some(checkHittingPlayer);
+  if (isGameOver) {
+    document.addEventListener('click', () => {
+      audio.pause();
     })
     audioDeath.play();
     wastedElement.style.display = "block";
     clearInterval(countIntervalId);
     clearInterval(spawnIntervalId);
     cancelAnimationFrame(animationId);
-    }
+  }
 
-    particles.forEach(particle => particle.update());
-    projectiles.forEach(projectile => projectile.update());
-    player.update();
-    enimies.forEach(enemy => enemy.update());
+  particles.forEach(particle => particle.update());
+  projectiles.forEach(projectile => projectile.update());
+  player.update();
+  enemies.forEach(enemy => enemy.update());
 }
 
 function projectileInsideWindow(projectile) {
-    return projectile.x + projectile.radius > 0 &&
-        projectile.x - projectile.radius < canvas.width &&
-        projectile.y - projectile.radius > 0 &&
-        projectile.y - projectile.radius < canvas.height
+  return projectile.x + projectile.radius > 0 &&
+    projectile.x - projectile.radius < canvas.width &&
+    projectile.y + projectile.radius > 0 &&
+    projectile.y - projectile.radius < canvas.height
 }
 
 function checkHittingPlayer(enemy) {
-    const distance = distanceBetweenTwoPoints(player.x, player.y, enemy.x, enemy.y);
-    return distance -enemy.radius - player.radius < 0; 
+  const distance = distanceBetweenTwoPoints(player.x, player.y, enemy.x, enemy.y);
+  return distance - enemy.radius - player.radius < 0;
 }
 
-function checkHittingEnemies(enemy) {
-    projectiles.some((projectile, index) => {
-        const distance = distanceBetweenTwoPoints(projectile.x, projectile.y, enemy.x, enemy.y);
-        if (distance - enemy.radius - projectile.radius > 0) return false;
+function checkHittingEnemy(enemy) {
+  projectiles.some((projectile, index) => {
+    const distance = distanceBetweenTwoPoints(projectile.x, projectile.y, enemy.x, enemy.y);
+    if (distance - enemy.radius - projectile.radius > 0) return false;
 
-        removeProjectileByIndex(index);
-        enemy.health--;
+    removeProjectileByIndex(index);
+    enemy.health--;
 
-        if (enemy.health < 1) {
-            increaseScore();
-            enemy.createExplosion(particles);
-        }
+    if (enemy.health < 1) {
+      increaseScore();
+      enemy.createExplosion(particles);
+    }
 
-        return true;
-    }) 
+    return true;
+  });
 }
 
 function removeProjectileByIndex(index) {
-    projectiles.splice(index, 1);
+  projectiles.splice(index, 1);
 }
 
 function increaseScore() {
-    score += 1;
-    scoreEl.innerHTML = score;
+  score += 250;
+  scoreEl.innerHTML = score;
 }
